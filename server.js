b4f8a3d20c1e4eb12dc53ebc173a1ad87423279e
Vivek Gupta <vivek.gupta85@gmail.com>
2016-04-03 17:53:10 -0500

    Updated language in server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(middleware.logger);
 });*/
 
 app.get('/about', middleware.requireAuthentication, function(req, res){
-	res.send('This is about us!!');
+	res.send('This is about us!!!!');
 });
 
 app.use(express.static(__dirname+'/public'));
